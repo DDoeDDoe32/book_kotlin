9e1727b055d1cddf9fde7e55e7e08e55c847d455
ddoeddoe32 <ddoeddoe32@naver.com>
2020-01-12 13:47:42 +0900

    first commit

---
+++ Hello/app/src/main/java/com/bong/hello/MainActivity.java
@@ -0,0 +1,42 @@
+package com.bong.hello;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 버튼이 클릭된 횟수를 저장하는 변수를 선언한다.
+    int clickCount = 0;
+
+    /**
+     * MainActivity 가 최초 실행될 때 시작한다.
+     * 자바독을 주석을 확인하려면 F1 키를 누르자.
+     * @param savedInstanceState
+     */
+
+
+    // MainActivity 가 최초 실행될 때 실행된다.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Activity 의 UI 를 R.layout.activit_main 으로 지정한다.
+        setContentView(R.layout.activity_main);
+
+        // "프로그래밍을 시작해보자!" 메세지를 잠시 보여준다.
+        Toast.makeText(getApplicationContext(), "프로그래밍을 시작해보자!", Toast.LENGTH_LONG).show();
+
+        // 레이아웃에 button 이라는 ID로 선언된 뷰에 클릭 이벤트 리스너를 등록한다.
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 클릭 카운트를 1회 증가시킨다.
+                clickCount = clickCount + 1;
+                // 버튼이 클릭되면 클릭된 횟수를 증가하면서 토스트 메세지를 보여준다.
+                Toast.makeText(getApplicationContext(), "clickCount: " + clickCount, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ Hello/app/src/main/java/com/bong/hello/SecondActivity.java
@@ -0,0 +1,52 @@
+package com.bong.hello;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    // 가속도가 3, 최고속도가 100, 브레이크 속도 4인 차를 만든다.
+    Car car1 = new Car(3, 100, 4);
+
+    // 가속도가 10, 최고속도가 50, 브레이크 속도 8인 차를 만든다.
+    SportsCar car2 = new SportsCar(10, 50, 8);
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // "프로그래밍을 시작해보자!" 메세지를 잠시 보여준다.
+        Toast.makeText(getApplicationContext(), "프로그래밍을 시작해보자!", Toast.LENGTH_LONG).show();
+
+        findViewById(R.id.testButton1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                car1.accelerationPedal();
+                car2.accelerationPedal();
+                String info = "car1: " + car1.getCurrentSpeedText() + ", car2:" + car2.getCurrentSpeedText();
+                Toast.makeText(getApplicationContext(), info, Toast.LENGTH_SHORT).show();
+
+                // 스포츠카의 선루프를 연다.
+                car2.openSunRoof();
+                Toast.makeText(getApplicationContext(), car2.getSunRoofInfo(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.testButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                car1.brakePedal();
+                car2.brakePedal();
+                String info = "car1: " + car1.getCurrentSpeedText() + ", car2:" + car2.getCurrentSpeedText();
+                Toast.makeText(getApplicationContext(), info, Toast.LENGTH_SHORT).show();
+
+                // 스포츠카의 선루프를 닫는다.
+                car2.closeSunRoof();
+                Toast.makeText(getApplicationContext(), car2.getSunRoofInfo(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
